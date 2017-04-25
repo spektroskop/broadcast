5fb7742de4114c254402867aafd8f75178743a2e
Alexander Stensrud <alex@stensrud.nu>
2017-04-25 09:56:37 +0200

    Context and receiver

--- broadcast.go
+++ broadcast.go
@@ -1,41 +1,89 @@
 package broadcast
 
-type channel chan packet
+import (
+	"context"
+)
 
-type packet struct {
-	c channel
-	v interface{}
+type (
+	Receiver struct {
+		ctx         context.Context
+		destination chan interface{}
+		source      Channel
+	}
+
+	Channel struct {
+		packets chan Packet
+		listen  chan Receiver
+	}
+
+	Packet struct {
+		packets chan Packet
+		value   interface{}
+	}
+)
+
+func open() chan Packet {
+	return make(chan Packet, 1)
 }
 
-func Channel() channel {
-	return make(channel, 1)
+func wrap(packets chan Packet, value interface{}) Packet {
+	return Packet{packets, value}
 }
 
-func wrap(value interface{}) packet {
-	return packet{Channel(), value}
+func (c *Channel) unwrap(p Packet) interface{} {
+	c.packets <- p
+	c.packets = p.packets
+	return p.value
 }
 
-func (c *channel) Fromp(receive chan interface{}) {
+func (c Channel) receive(r Receiver) {
 	for {
-		p := wrap(<-receive)
-		*c <- p
-		*c = p.c
+		select {
+		case <-r.ctx.Done():
+			return
+		case p := <-c.packets:
+			r.destination <- c.unwrap(p)
+		}
 	}
 }
 
-func (c channel) From(receive chan interface{}) {
-	for {
-		p := wrap(<-receive)
-		c <- p
-		c = p.c
+func New() Channel {
+	return Channel{
+		packets: open(),
+		listen:  make(chan Receiver),
 	}
 }
 
-func (c channel) Into(send chan<- interface{}) {
+func (c Channel) From(ctx context.Context, source <-chan interface{}) {
 	for {
-		p := <-c
-		c <- p
-		c = p.c
-		send <- p.v
+		select {
+		case <-ctx.Done():
+			return
+		case value := <-source:
+			c.unwrap(wrap(open(), value))
+		case receiever := <-c.listen:
+			go c.receive(receiever)
+		}
 	}
+}
+
+func (c Channel) Into(ctx context.Context, d chan interface{}) {
+	c.listen <- Receiver{ctx: ctx, destination: d}
+}
+
+func (c Channel) Receiver(d chan interface{}) Receiver {
+	return Receiver{
+		ctx:         context.Background(),
+		destination: d,
+		source:      c,
+	}
+}
+
+func (r Receiver) Listen(ctx context.Context) {
+	r.ctx = ctx
+	r.source.listen <- r
+}
+
+func (r Receiver) Destination() <-chan interface{} {
+	return r.destination
 }
